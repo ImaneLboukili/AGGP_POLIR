66e73b94ceed76c2895a5fd0a14722eb934ca15a
Romain-B <romain.bulteau@insa-lyon.fr>
2018-03-07 13:36:45 +0100

    started testing Individu class

--- main.py
+++ main.py
@@ -2,8 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import time
-
+from Individu import Individu
+import matplotlib.pyplot as plt
 #----------------------
 
-if __name__ == '__main__':
-  pass
+
+
+plt.style.use('bmh')
+
+
+I = Individu(1000, 2)
+print I.fat
+I.plot_degree_hist()
