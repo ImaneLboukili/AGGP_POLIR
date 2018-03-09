76c28e7ff577dc9d5f3ad8ba4c978316c46a4e09
Romain-B <romain.bulteau@insa-lyon.fr>
2018-03-09 15:53:51 +0100

    Started the Population class

--- Population.py
+++ Population.py
@@ -4,5 +4,14 @@
 import networkx
 import time
 
+from Individu import Individu
 #----------------------
 
+class Population(object):
+  """docstring for Population"""
+  def __init__(self, nInd, N, M):
+    
+    self.nInd = nInd
+    self.individus = [Individu(N, M) for _ in range(self.nInd)]
+    
+    
