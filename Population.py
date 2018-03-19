703377b4e86143464035f572ea54ff9aaeead46e
Romain-B <romain.bulteau@insa-lyon.fr>
2018-03-19 22:32:11 +0100

    Added some doc and echsize attribute

--- Population.py
+++ Population.py
@@ -26,17 +26,31 @@ from multiprocessing import Pool
 
 # because of Pool
 def mk_i(nm):
-  return Individu(nm[0], nm[1])
+  return Individu(nm[0], nm[1], nm[2])
 
 
 
 class Population(object):
-  """docstring for Population"""
-  def __init__(self, nInd, N, M, method="random", logfile=False, nprocess = 3):
+  """docstring for Population
+  Parameters :
+    nInd  = number of Individus in population
+    N     = number of nodes in Individus (given to constructor of Individu)
+    M     = number of edges addes per node in BA method ('' '')
+    params = parameters of the ideal network we're trying to converge towards (ga, dm, cc) ('' '')
+    echsize = size of selected sample used to produce next generation
+    method = method of selection of the sample 'lion', 'roulette' or 'random'
+    logfile = name.path to of the logfile in which progress is written
+    nprocess = number of processors to use for multiprocess parts
+
+  """
+  def __init__(self, nInd, N, M, params=(1,1,1), echsize=nInd/2,  method="random", logfile=False, nprocess = 3):
     
     self.nInd = nInd
+    self.echsize = echsize
+    self.params = params
     self.method = method
     self.pool = Pool(nprocess)
+
 
     self.logfile = logfile
     if self.logfile :
@@ -48,21 +62,21 @@ class Population(object):
           "\n## "+str(datetime.datetime.now())+" : Simulation started."+\
           "\n## Parameters are the following :" +\
           "\n##      nInd = "+str(self.nInd)+"  N = "+str(N)+"  M = "+str(M) +\
-          "\n##      method = "+self.method+\
+          "\n##      g_th = "+str('%.2f'%self.params[0])+"  dmoy = "+str('%.2f'%self.params[1])+ "  cc = "+str('%.2f'%self.params[2])+\
+          "\n##      echsize = "+str(self.echsize)+"  method = "+self.method+\
           "\n##      nprocess = "+str(nprocess)
     
     self.wlog(log)
 
     # Population generation
+    # basically [Individu(N, M) for _ in range(self.nInd)] but in multiprocess.
     start_gen = time.time()
-    self.pop = []   #[Individu(N, M) for _ in range(self.nInd)]
-
+    self.pop = []
     # make an iterable array of parameters for every Indiv.
-    pms = [(N, M)]*self.nInd 
+    pms = [(N, M, self.params)]*self.nInd 
 
     # get the results
     self.pop = self.pool.map(mk_i, pms)
-
     init_gen_time = time.time()-start_gen
 
 
@@ -70,7 +84,12 @@ class Population(object):
           "\n##      Done in "+str('%.2f'%init_gen_time)+"s\n"
     self.wlog(log)
 
+
+    # selected individuals to give next gen
     self.ech = None
+
+    #current gen
+    self.gen = 0
 
 
 
@@ -82,9 +101,11 @@ class Population(object):
     else :
       print log
     
+  def generation(self):
+    pass
     
-  def selection(self, N):
-  	# selects N individuals, which probability of selection is based on their fatness
+  def selection(self):
+  	# selects self.echsize individuals, which probability of selection is based on their fatness
   	if "roulette" == self.method:
 
   		sum_fat = sum([i.fat for i in self.pop])
@@ -93,7 +114,7 @@ class Population(object):
   		self.ech = []
   		ind = None
 
-  		while len(self.ech)<N :
+  		while len(self.ech)<self.echsize :
   			while ind in selected_ind or ind == None:
   				
   				ind = rd.randint(0, self.nInd-1)
@@ -105,22 +126,22 @@ class Population(object):
   				
   		
   	if "lion" == self.method:
-  		#returns the N individuals with the lowest fatness
+  		#returns the self.echsize individuals with the lowest fatness
   		rank = np.argsort([i.fat for i in pop])
-  		self.ech = [pop[r] for r in rank[0:N]]
+  		self.ech = [pop[r] for r in rank[0:self.echsize]]
 
   		
   	if "random" == self.method :
-  		#returns N random individuals
-  		self.ech = rd.sample(pop, N)
+  		#returns self.echsize random individuals
+  		self.ech = rd.sample(pop, self.echsize)
 
   def crossing_over(self, A, B):
     # A and B are 2 Individu objects
-    n = rd.randint(0, int((A.N-1)/2))
+    n = rd.randint(0, int((A.self.echsize-1)/2))
     C = A.copy()
 
-    nodes_A = [rd.randint(0, A.N-1) for _ in range(n)]
-    nodes_B = [rd.randint(0, B.N-1) for _ in range(n)]
+    nodes_A = [rd.randint(0, A.self.echsize-1) for _ in range(n)]
+    nodes_B = [rd.randint(0, B.self.echsize-1) for _ in range(n)]
 
     new_edges = []
     del_edges = []
