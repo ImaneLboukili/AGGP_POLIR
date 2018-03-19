cab7cfdaa394ea9e230c45e3d11aea3199b0f8bb
Romain-B <romain.bulteau@insa-lyon.fr>
2018-03-19 22:19:05 +0100

    Got rid of powerlaw warnings

--- Individu.py
+++ Individu.py
@@ -4,6 +4,8 @@
 import networkx as nx
 import time
 import powerlaw
+import sys
+import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
@@ -29,7 +31,9 @@ class Individu(object):
     return Individu(self.N, self.M, param=self.param, copy=True, G = self.G.copy(), fat = self.fat)
 
   def fatness(self):
-    g_obs = powerlaw.Fit(nx.degree_histogram(self.G)).alpha
+    with warnings.catch_warnings():
+      warnings.simplefilter("ignore")
+      g_obs = powerlaw.Fit(nx.degree_histogram(self.G), verbose=False).alpha
     cc = nx.average_clustering(self.G)
     dmoy = nx.average_shortest_path_length(self.G)
     fatness = sum([(1-g_obs/self.param[0])**2, (dmoy/self.param[1])*2,(cc/self.param[2])**2])
